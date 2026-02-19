a98e4431c5f7fad50defe32e431d084e8e82f935
rakibul875 <rakibulislamashik78@gmail.com>
2026-02-19 15:18:53 +0600

    funtion loggin form

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById("Login-button").addEventListener('click',function(){
+    const numberInput=document.getElementById("input-number")
+    const number=numberInput.value
+
+    const pinInput=document.getElementById("input-pin")
+    const pin=pinInput.value
+    if(number==="01911225544" && pin==="1234"){
+        alert("Login Success")
+        window.location.assign("Home.html")
+    }
+    else{
+        alert("Login Field")
+        return
+    }
+})
